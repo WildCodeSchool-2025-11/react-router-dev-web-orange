d735e422b372493427e313b898b3130ffa4e053d
yavuzkutuk <yavuz@kutuk.fr>
2026-01-15 10:31:57 +0100

    ADD : fetch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
 // Home.tsx
+import { useState, useEffect } from "react";
 
 function Home() {
-  return <h1>Hello from Home</h1>;
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+     fetch("chemin-de-la-reussite")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      setItem(data[0])   
+    });
+  }, [])
+  
+
+  if (item == null) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <h1>Affichage d'un {item?.nom}</h1>
+      
+    </>
+  );
 }
 
 export default Home;
